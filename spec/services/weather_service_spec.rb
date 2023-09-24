fdf05316f9e3301eaf44291df23776759457b80b
Artemy Gibson <artemygibson@gmail.com>
2023-09-23 19:56:45 -0700

    Test that weather service can return current weather for a given location

--- spec/services/weather_service_spec.rb
+++ spec/services/weather_service_spec.rb
@@ -1,16 +1,28 @@
 require "rails_helper"
 
-RSpec.describe WeatherService do
+RSpec.describe WeatherService, :vcr do
   it "connects to the weather API" do
     service = WeatherService.new
 
     expect(service.conn).to be_a(Faraday::Connection)
   end
 
-  xit "returns current weather for a given city" do
+  it "returns current weather for a given city" do
     service = WeatherService.new
 
     response = service.current_weather("cincinatti,oh")
 
+    data = JSON.parse(response.body, symbolize_names: true)
+    expect(data).to have_key(:location)
+    expect(data).to have_key(:current)
+    expect(data[:current]).to have_key(:last_updated)
+    expect(data[:current]).to have_key(:temp_f)
+    expect(data[:current]).to have_key(:feelslike_f)
+    expect(data[:current]).to have_key(:humidity)
+    expect(data[:current]).to have_key(:uv)
+    expect(data[:current]).to have_key(:vis_miles)
+    expect(data[:current]).to have_key(:condition)
+    expect(data[:current][:condition]).to have_key(:text)
+    expect(data[:current][:condition]).to have_key(:icon)
   end
 end
