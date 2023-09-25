7d191e2a73f7dcaa17d4f110383195111d329f7f
Artemy Gibson <artemygibson@gmail.com>
2023-09-25 14:55:57 -0700

    Create road trip controller with create, strong params, and validate_key methods

---
+++ app/controllers/api/v0/road_trip_controller.rb
@@ -0,0 +1,15 @@
+class Api::V0::RoadTripController < ApplicationController
+  before_action :validate_key
+
+  def create 
+    render json: RoadTripSerializer.new(RoadTripFacade.new(road_trip_params))
+  end
+
+  def road_trip_params
+    params.permit(:origin, :destination)
+  end
+
+  def validate_key
+    User.find_by!(api_key: params[:api_key])
+  end
+end
