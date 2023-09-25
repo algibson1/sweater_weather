d75a64d38ab41bbd867103987b6519fb61d37d10
Artemy Gibson <artemygibson@gmail.com>
2023-09-25 09:47:20 -0700

    Add to_json method to book search serializer to format search results

---
+++ app/serializers/book_search_serializer.rb
@@ -0,0 +1,24 @@
+class BookSearchSerializer 
+  def initialize(forecast, book_results)
+    @forecast = forecast[:current]
+    @book_results = book_results
+  end
+
+  def to_json 
+    { 
+      data: {
+        id: nil,
+        type: "books",
+        attributes: {
+          destination: @book_results[:destination],
+          forecast: {
+            summary: @forecast[:condition][:text],
+            temperature: "#{@forecast[:temp_f].round} F"
+          },
+          total_books_found: @book_results[:total_books_found],
+          books: @book_results[:books]
+        }
+      }
+    }
+  end
+end
