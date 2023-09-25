76069dc732e2516996ae113fe46063f0ddb9dd67
Artemy Gibson <artemygibson@gmail.com>
2023-09-25 09:07:57 -0700

    Add search method to open library service

--- app/services/open_library_service.rb
+++ app/services/open_library_service.rb
@@ -1,7 +1,12 @@
 class OpenLibraryService
   def conn 
-    Faraday.new("https://openlibrary.org/search.json")
+    Faraday.new("https://openlibrary.org/")
   end
 
-  
+  def search(location)
+    conn.get("search.json") do |faraday|
+      faraday.params[:q] = location
+      faraday.params[:fields] = "title,isbn"
+    end
+  end
 end
