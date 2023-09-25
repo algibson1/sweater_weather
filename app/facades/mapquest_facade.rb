8bad93f67150d80549301939438c8ca54d61e16c
Artemy Gibson <artemygibson@gmail.com>
2023-09-25 15:48:11 -0700

    Create get_directions method for mapquest facade

--- app/facades/mapquest_facade.rb
+++ app/facades/mapquest_facade.rb
@@ -4,6 +4,11 @@ class MapquestFacade
     JSON.parse(response.body, symbolize_names: true)[:results][0][:locations][0][:displayLatLng]
   end
 
+  def get_directions(locations)
+    response = service.get_directions(locations)
+    JSON.parse(response.body, symbolize_names: true)[:route]
+  end
+
   def service 
     @service ||= MapquestService.new
   end
