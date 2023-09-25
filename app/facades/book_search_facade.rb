f8d242118051ccaac84e25209ce02ae039736b53
Artemy Gibson <artemygibson@gmail.com>
2023-09-25 09:26:49 -0700

    Create book search facade find_books method

---
+++ app/facades/book_search_facade.rb
@@ -0,0 +1,15 @@
+class BookSearchFacade 
+  def find_books(query_params)
+    response = service.search(query_params[:location])
+    results = JSON.parse(response.body, symbolize_names: true)
+    {
+      total_books_found: results[:numFound],
+      destination: query_params[:location],
+      books: results[:docs][0...query_params[:quantity]]
+    }
+  end
+
+  def service 
+    @book_service ||= OpenLibraryService.new
+  end
+end
