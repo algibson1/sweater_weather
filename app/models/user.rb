48a69567e037ddaaf54dbb116a1b287ac6804c88
Artemy Gibson <artemygibson@gmail.com>
2023-09-24 18:02:53 -0700

    Create user model

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord 
+  validates_presence_of :email, :password, :api_key
+  validates :email, uniqueness: { case_sensitive: false }
+
+  has_secure_password
+
+  def generate_key 
+    chars = ('a'..'z').to_a + ('A'..'Z').to_a + (0..9).to_a
+    update(api_key: chars.shuffle.take(32).join(""))
+  end
+end
