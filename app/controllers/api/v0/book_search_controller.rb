37c892f282a7f8f4b7af79e205c0b649bde705ac
Artemy Gibson <artemygibson@gmail.com>
2023-09-25 10:40:59 -0700

    Add actions to validate search query and return error message

--- app/controllers/api/v0/book_search_controller.rb
+++ app/controllers/api/v0/book_search_controller.rb
@@ -1,4 +1,8 @@
-class Api::V0::BookSearchController < ApplicationController 
+class Api::V0::BookSearchController < ApplicationController
+  before_action :validate_location
+  before_action :validate_quantity
+  rescue_from ActionController::BadRequest, with: :bad_request_response
+
   def index 
     forecast = WeatherFacade.new.get_forecast(params[:location])
     book_results = BookSearchFacade.new.find_books(search_params)
@@ -9,5 +13,21 @@ class Api::V0::BookSearchController < ApplicationController
 
   def search_params
     params.permit(:location, :quantity)
+  end
+
+  def validate_quantity 
+    unless params[:quantity].to_i > 0
+      raise ActionController::BadRequest.new("Quantity must be a number greater than 0")
+    end
+  end
+
+  def validate_location 
+    unless params[:location]
+      raise ActionController::BadRequest.new("Location cannot be blank")
+    end
+  end
+
+  def bad_request_response(error)
+    render json: {message: error.message}, status: :unprocessable_entity
   end
 end
