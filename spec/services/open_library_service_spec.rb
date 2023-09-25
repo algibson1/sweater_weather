22bc35424d23baf09ec314be2b1ccd1a1a3ed563
Artemy Gibson <artemygibson@gmail.com>
2023-09-25 09:07:39 -0700

    Finish full test for expected return from open library api search endpoint

--- spec/services/open_library_service_spec.rb
+++ spec/services/open_library_service_spec.rb
@@ -7,14 +7,30 @@ RSpec.describe OpenLibraryService do
     expect(service.conn).to be_a(Faraday::Connection)
   end
 
-  it "searches books by location" do
+  it "searches books by location", :vcr do
     service = OpenLibraryService.new
 
-    response = service.search({location: "denver,co"})
+    response = service.search("denver,co")
 
     results = JSON.parse(response.body, symbolize_names: true)
     
-    expect(results).to have_key(:numfound)
-    
+    expect(results).to have_key(:numFound)
+    expect(results[:numFound]).to be_an(Integer)
+
+    expect(results).to have_key(:docs)
+
+    books = results[:docs]
+    expect(books).to be_an(Array)
+    expect(books.count <= results[:numFound]).to eq(true)
+
+    books.each do |book|
+      expect(book).to have_key(:title)
+      expect(book[:title]).to be_a(String)
+      if book[:isbn]
+        expect(book[:isbn]).to be_an(Array)
+        expect(book[:isbn]).to all be_a(String)
+      end
+      expect(book.count).to eq(1).or eq(2)
+    end
   end
 end
