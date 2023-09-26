25ff1ca07a86b83db8339cdb99e854182204e977
Artemy Gibson <artemygibson@gmail.com>
2023-09-26 09:57:07 -0700

    Move record invalid error handling to ApplicationController

--- app/controllers/api/v0/users_controller.rb
+++ app/controllers/api/v0/users_controller.rb
@@ -2,13 +2,8 @@ class Api::V0::UsersController < ApplicationController
   def create 
     user = User.new(user_params)
     user.generate_key
-    begin 
-      user.save!
-      render json: UsersSerializer.new(user), status: :created
-    rescue ActiveRecord::RecordInvalid => error 
-      return render json: ErrorSerializer.new(error).to_json, status: :unprocessable_entity if error.message.include?("taken") 
-      render json: ErrorSerializer.new(error).to_json, status: :bad_request 
-    end
+    user.save!
+    render json: UsersSerializer.new(user), status: :created
   end
 
   private 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::API
   rescue_from ActionController::BadRequest, with: :bad_request_response
   rescue_from ActiveRecord::RecordNotFound, with: :unauthorized_response
+  rescue_from ActiveRecord::RecordInvalid, with: :invalid_response
 
   def bad_request_response(error)
     render json: ErrorSerializer.new(error).to_json, status: :bad_request
@@ -8,5 +9,10 @@ class ApplicationController < ActionController::API
 
   def unauthorized_response(error)
     render json: ErrorSerializer.new(error).to_json, status: :unauthorized
+  end
+
+  def invalid_response(error)
+    return render json: ErrorSerializer.new(error).to_json, status: :unprocessable_entity if error.message.include?("taken") 
+    return bad_request_response(error)
   end
 end
