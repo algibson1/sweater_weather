2ce25dce7f7509b032f6304459ede7f059a2608b
Artemy Gibson <artemygibson@gmail.com>
2023-09-25 19:50:33 -0700

    Create road trip facade

---
+++ app/facades/road_trip_facade.rb
@@ -0,0 +1,20 @@
+class RoadTripFacade 
+  def initialize(locations)
+    @locations = locations
+  end
+
+  def get_trip_info 
+    directions = maps.get_directions(@locations)
+    forecast = weather.get_forecast(nil, directions[:boundingBox][:ul])
+    require 'pry'; binding.pry
+    RoadTrip.new(directions, forecast)
+  end
+
+  def maps 
+    @mapquest ||= MapquestFacade.new 
+  end
+
+  def weather 
+    @weather ||= WeatherFacade.new
+  end
+end
