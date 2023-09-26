5726e7c5bc77876f978e7845bc9ef9197bf55690
Artemy Gibson <artemygibson@gmail.com>
2023-09-26 08:41:38 -0700

    Change api key error message, include trip status in response

--- app/controllers/api/v0/road_trip_controller.rb
+++ app/controllers/api/v0/road_trip_controller.rb
@@ -1,14 +1,15 @@
 class Api::V0::RoadTripController < ApplicationController
-  before_action :validate_key
+  before_action :validate_key, only: [:create]
   rescue_from ActiveRecord::RecordNotFound, with: :unauthorized_response
 
   def create 
-    render json: RoadTripSerializer.new(facade.get_trip_info)
+    render json: RoadTripSerializer.new(trip), status: trip.status
   end
 
   private
-  def facade 
-    @facade = RoadTripFacade.new(road_trip_params)
+
+  def trip 
+    @trip ||= RoadTripFacade.new(road_trip_params).get_trip_info
   end
 
   def road_trip_params
@@ -17,7 +18,7 @@ class Api::V0::RoadTripController < ApplicationController
 
   def validate_key
     unless User.find_by(api_key: params[:api_key])
-      raise ActiveRecord::RecordNotFound, "Validation failed: Api key is not valid"
+      raise ActiveRecord::RecordNotFound, "Validation failed: Api key is missing or invalid"
     end
   end
 
