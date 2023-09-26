b8e2a4767f2ffe9bdd75107be28905dafac539f1
Artemy Gibson <artemygibson@gmail.com>
2023-09-26 08:37:44 -0700

    Add test case for multi-day road trips

--- spec/requests/api/v0/road_trip_request_spec.rb
+++ spec/requests/api/v0/road_trip_request_spec.rb
@@ -96,51 +96,55 @@ RSpec.describe "Road trip endpoint", :vcr do
   it "has empty info if there is no route between the cities" do
     
     params = {
-      "origin": "Cincinatti,OH",
-      "destination": "London,England",
+      "origin": "Cincinatti, OH",
+      "destination": "London, England",
       "api_key": @user.api_key
     }    
 
     post "/api/v0/road_trip", params: params.to_json, headers: {'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json'}
 
-    expect(response.status).to eq(402)
+    expect(response.status).to eq(422)
+
+    data = JSON.parse(response.body, symbolize_names: true)
+
+    expected = {
+      data: {
+        id: nil,
+        type: "road_trip",
+        attributes: {
+          start_city: "Cincinatti, OH",
+          end_city: "London, England",
+          travel_time: "impossible",
+          weather_at_eta: {}
+        }
+      }
+    }
+
+    expect(data).to eq(expected)
+  end
+
+  it "can do BIG road trips" do
+    params = {
+      "origin": "Anchorage,Alaska",
+      "destination": "Panama City, Panama",
+      "api_key": @user.api_key
+    }    
+
+    post "/api/v0/road_trip", params: params.to_json, headers: {'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json'}
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
 
     data = JSON.parse(response.body, symbolize_names: true)[:data]
 
-    expect(data).to have_key(:id)
-    expect(data[:id]).to eq(nil)
-
-    expect(data).to have_key(:type)
-    expect(data[:type]).to eq("road_trip")
-    
-    expect(data).to have_key(:attributes)
-    expect(data[:attributes]).to be_a(Hash)
-
-    expect(data[:attributes]).to have_key(:start_city)
-    expect(data[:attributes][:start_city]).to eq("Cincinnati, OH")
-
-    expect(data[:attributes]).to have_key(:end_city)
-    expect(data[:attributes][:end_city]).to eq("Chicago, IL")
+    expect(data[:attributes][:start_city]).to eq("Anchorage, AK")
+    expect(data[:attributes][:end_city]).to eq("Ciudad de Panamá, PA")
 
     expect(data[:attributes]).to have_key(:travel_time)
     expect(data[:attributes][:travel_time]).to be_a(String)
-    expect(data[:attributes][:travel_time]).to match(/\d{2}:\d{2}/)
+    expect(data[:attributes][:travel_time]).to match(/\d{3}:\d{2}/) #expecting 100+ hours
 
-    expect(data[:attributes]).to have_key(:weather_at_eta)
-    expect(data[:attributes][:weather_at_eta]).to be_a(Hash)
-
-    expect(data[:attributes][:weather_at_eta]).to have_key(:datetime)
-    expect(data[:attributes][:weather_at_eta][:datetime]).to be_a(String)
     expect(data[:attributes][:weather_at_eta][:datetime]).to match(/\d{4}-\d{2}-\d{2} \d{2}:\d{2}/)
-
-    expect(data[:attributes][:weather_at_eta]).to have_key(:temperature)
-    expect(data[:attributes][:weather_at_eta][:temperature]).to be_a(Float)
-    
-    expect(data[:attributes][:weather_at_eta]).to have_key(:condition)
-    expect(data[:attributes][:weather_at_eta][:condition]).to be_a(String)
-  end
-
-  xit "can do BIG road trips" do
-    #Alaska to Panama?
+    expect(data[:attributes][:weather_at_eta][:datetime].to_date > (Time.now + 4.days)).to eq(true)
   end
 end
