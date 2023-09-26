0889483b6757141e6d8a6c169c55286c0b4f61ac
Artemy Gibson <artemygibson@gmail.com>
2023-09-26 08:40:05 -0700

    Change get_forcast to accept days argument in facade

--- app/facades/weather_facade.rb
+++ app/facades/weather_facade.rb
@@ -1,6 +1,6 @@
 class WeatherFacade 
-  def get_forecast(location, coordinates = nil)
-    response = service.forecast(coordinates || get_coordinates(location))
+  def get_forecast(location, coordinates = nil, days = 5)
+    response = service.forecast(coordinates || get_coordinates(location), days)
     JSON.parse(response.body, symbolize_names: true)
   end
   
