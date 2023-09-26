1254b085d491842b5fcb4ac76d1e859918e4247c
Artemy Gibson <artemygibson@gmail.com>
2023-09-26 09:42:15 -0700

    Move error handling into ApplicationController

--- app/controllers/api/v0/forecast_controller.rb
+++ app/controllers/api/v0/forecast_controller.rb
@@ -1,12 +1,7 @@
 class Api::V0::ForecastController < ApplicationController
-  rescue_from ActionController::BadRequest, with: :bad_request_response
   
   def index
     forecast = WeatherFacade.new.get_forecast(params[:location])
     render json: ForecastSerializer.new(forecast).to_json
-  end
-
-  def bad_request_response(error)
-    render json: ErrorSerializer.new(error).to_json, status: :bad_request
   end
 end
