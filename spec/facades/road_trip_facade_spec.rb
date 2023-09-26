2ca1eb5be3f5e3f02a4838e37507ac1d970fa48f
Artemy Gibson <artemygibson@gmail.com>
2023-09-25 21:33:01 -0700

    Add test for facade sad path: road trip route doesn't exist

--- spec/facades/road_trip_facade_spec.rb
+++ spec/facades/road_trip_facade_spec.rb
@@ -13,4 +13,16 @@ RSpec.describe RoadTripFacade, :vcr do
     expect(trip.weather_at_eta).to be_a(Hash)
     expect(trip.weather_at_eta.keys).to match_array([:datetime, :temperature, :condition])
   end
+
+  it "returns a partial poro if no directions are available" do
+    facade = RoadTripFacade.new({origin: "cincinatti,oh", destination: "paris,france"})
+
+    bad_trip = facade.get_trip_info
+
+    expect(bad_trip).to be_a(RoadTrip)
+    expect(bad_trip.start_city).to eq("cincinatti,oh")
+    expect(bad_trip.end_city).to eq("paris,france")
+    expect(bad_trip.travel_time).to eq("impossible")
+    expect(bad_trip.weather_at_eta).to eq({})
+  end
 end
