2b6ded650cb32f95d31858989cbfd1768ffdaed3
Artemy Gibson <artemygibson@gmail.com>
2023-09-25 19:55:12 -0700

    Finish test for road trip facade get_trip_info method

--- spec/facades/road_trip_facade_spec.rb
+++ spec/facades/road_trip_facade_spec.rb
@@ -7,8 +7,10 @@ RSpec.describe RoadTripFacade, :vcr do
     trip = facade.get_trip_info
 
     expect(trip).to be_a(RoadTrip)
-    # expect distance 
-    # weather 
-    # etc
+    expect(trip.start_city).to eq("Cincinnati, OH")
+    expect(trip.end_city).to eq("Chicago, IL")
+    expect(trip.travel_time).to be_a(String)
+    expect(trip.weather_at_eta).to be_a(Hash)
+    expect(trip.weather_at_eta.keys).to match_array([:datetime, :temperature, :condition])
   end
 end
